065e9ce6917603a847c46b208b5614a8143012c9
Will-Parrott <willchat@vt.edu>
2026-04-28 20:47:09 -0400

    Updated RFTurnGreen and created Reforecast Script.

--- ReforecastScript.R
+++ ReforecastScript.R
@@ -13,13 +13,9 @@ install.packages("ranger")
 
 #Bring in necessary packages
 library(tidyverse)
-print("tidyverse")
 library(tidymodels)
-print("tidymodels")
 library(lubridate)
-print("lubridate")
 library(ranger)
-print("ranger")
 
 #Bring in targets
 print("Reading site_date")
@@ -55,6 +51,7 @@ for(i in 1:length(focal_sites)){
   weather_past <- weather_past_s3 |> 
     filter(site_id == focal_site,
            datetime >= ymd('2017-01-01'),
+           datetime <= ymd('2023-12-31'),
            variable %in% met_variables) |> 
     dplyr::collect()
   
@@ -70,35 +67,12 @@ for(i in 1:length(focal_sites)){
 }
 
 
-# Future weather
-print("Forecast and noaa date")
-forecast_date = Sys.Date()
-noaa_date = forecast_date - days(1)
-
 #Forecast parameters
+ref_dates <- seq(ymd("2024-01-01"), ymd("2024-12-31"), by = "2 week")
 forecast_horizon <- 30
-forecasted_dates <- seq(from = ymd(forecast_date), to = ymd(forecast_date) + forecast_horizon, by = "day")
-summer <- seq(from = 6, to = 9)
-winter <- c(12, 1, 2, 3)
-n_members <- 200
+n_members <- 31
 
-weather_future_s3 <- neon4cast::noaa_stage2(start_date = as.character(noaa_date-1)) #-1 is a temp fix while stage 2 is being dumb
 
-weather_future <- weather_future_s3 |> 
-  dplyr::filter(datetime >= forecast_date,
-                site_id %in% focal_sites,
-                variable %in% met_variables) |> 
-  collect()
-
-weather_future_daily <- weather_future |> 
-  mutate(datetime = as_date(datetime)) |> 
-  # mean daily forecasts at each site per ensemble
-  group_by(datetime, site_id, parameter, variable) |> 
-  summarize(prediction = mean(prediction, na.rm = TRUE), .groups = "drop") |> 
-  # convert air temperature to Celsius if it is included in the weather data
-  mutate(prediction = ifelse(variable == "air_temperature", prediction - 273.15, prediction)) |> 
-  pivot_wider(names_from = variable, values_from = prediction) |> 
-  select(any_of(c('datetime', 'site_id', met_variables, 'parameter')))
 
 
 #///////////////////////////////////////////////////////////UNCERTAINTY//////////////////////////////////////////////////////////////////
@@ -185,7 +159,7 @@ for(s in 1:length(focal_sites)){
   metric_table <- metric_table |> 
     pivot_wider(names_from = .metric, values_from = .estimate)
   model_performance <- model_performance |> add_row(site_id = focal_sites[s], rsq = metric_table$rsq)
-
+  
   ggplot(gcc_pred_test, aes(x=gcc_90, y=.pred)) + geom_point()
   metric_table
   
@@ -199,85 +173,97 @@ for(s in 1:length(focal_sites)){
   targets_unc_ref <- targets_2025 |> filter(as.Date(datetime) %in% stable_per)
   init_sd <- sd(targets_unc_ref$observation, na.rm = TRUE)
   init_uc_df <- rnorm(n_members, mean = 0, sd = init_sd)
-  
-  for(d in 1:length(forecasted_dates)){
+  for(i in 1:length(ref_dates)){
+    forecasted_dates = seq(ymd(ref_dates[i]), ymd(ref_dates[i]+forecast_horizon), by = "day")
     
-    met_ens_id <- 0
-    for(ens in 1:n_members){
-      print(paste0(forecasted_dates[d], "-", ens))
+    weather_future_s3 <- neon4cast::noaa_stage2(start_date = as.character(ref_dates[i])) #-1 is a temp fix while stage 2 is being dumb
+    
+    weather_future <- weather_future_s3 |> 
+      dplyr::filter(datetime %in% forecasted_dates,
+                    site_id == focal_sites[s],
+                    variable %in% met_variables) |> 
+      collect()
+    
+    weather_future_daily <- weather_future |> 
+      mutate(datetime = as_date(datetime)) |> 
+      # mean daily forecasts at each site per ensemble
+      group_by(datetime, site_id, parameter, variable) |> 
+      summarize(prediction = mean(prediction, na.rm = TRUE), .groups = "drop") |> 
+      # convert air temperature to Celsius if it is included in the weather data
+      mutate(prediction = ifelse(variable == "air_temperature", prediction - 273.15, prediction)) |> 
+      pivot_wider(names_from = variable, values_from = prediction) |> 
+      select(any_of(c('datetime', 'site_id', met_variables, 'parameter')))
+    
+    for(d in 1:length(forecasted_dates)){
       
-      if(met_ens_id <= 30){
-        met_ens_id <- met_ens_id + 1
-        ens_nm <- paste0(ens, "-", met_ens_id)
-      }else{
-        met_ens_id <- 1
+      met_ens_id <- 0
+      for(ens in 1:n_members){
+        print(paste0(forecasted_dates[d], "-", ens))
+        
+        if(met_ens_id <= 30){
+          met_ens_id <- met_ens_id + 1
+          ens_nm <- paste0(ens, "-", met_ens_id)
+        }else{
+          met_ens_id <- 1
+        }
+        
+        met_ens <- weather_ensemble_names[met_ens_id]
+        
+        if(d == 1){ #set current lagged value - last available data for d=1, otherwise use last available prediction
+          lag_curr <- targets_m$lag_gcc[nrow(targets_m)] + init_uc_df[ens+1]
+        }else{
+          curr_ens_pred <- forecast_df |> filter(datetime == forecasted_dates[d-1], 
+                                                 reference_datetime == ref_dates[i], 
+                                                 parameter == ens,
+                                                 site_id == focal_sites[s])
+          lag_curr <- curr_ens_pred$prediction[nrow(curr_ens_pred)]
+        }
+        
+        #Establish prediction dataframe with necessary components
+        pred_df <- weather_future_daily |> 
+          filter(datetime == as.Date(forecasted_dates[d]), site_id == focal_sites[s], parameter == met_ens) |>
+          mutate(lag_gcc = lag_curr,
+                 project_id = unique(targets_m$project_id),
+                 duration = unique(targets_m$duration))
+        
+        forecasted_gcc <- predict(gcc_fit, new_data = pred_df) + process_df[ens]
+        
+        curr_site_df <- tibble(datetime = pred_df$datetime,
+                               reference_datetime = ref_dates[i],
+                               site_id = focal_sites[s],
+                               parameter = ens, #make sure this uses ens, not met_ens. Plot breaks if done latter way.
+                               prediction = forecasted_gcc[[1]],
+                               variable = "gcc_90")
+        forecast_df<- as.data.frame(rbind(forecast_df, curr_site_df)) 
+        
+        #the targets data only goes to 2024, remove the na.rm() thing from the targets.m call.
       }
-      
-      met_ens <- weather_ensemble_names[met_ens_id]
-      
-      if(d == 1){ #set current lagged value - last available data for d=1, otherwise use last available prediction
-        lag_curr <- targets_m$lag_gcc[nrow(targets_m)] + init_uc_df[ens+1]
-      }else{
-        curr_ens_pred <- forecast_df |> filter(datetime == forecasted_dates[d-1], parameter == ens)
-        lag_curr <- curr_ens_pred$prediction[nrow(curr_ens_pred)]
-      }
-      
-      #Establish prediction dataframe with necessary components
-      pred_df <- weather_future_daily |> 
-        filter(datetime == as.Date(forecasted_dates[d]), site_id == focal_sites[s], parameter == met_ens) |>
-        mutate(lag_gcc = lag_curr,
-               project_id = unique(targets_m$project_id),
-               duration = unique(targets_m$duration))
-      
-      forecasted_gcc <- predict(gcc_fit, new_data = pred_df) + process_df[ens]
-      
-      curr_site_df <- tibble(datetime = pred_df$datetime,
-                             reference_datetime = forecast_date,
-                             site_id = focal_sites[s],
-                             parameter = ens, #make sure this uses ens, not met_ens. Plot breaks if done latter way.
-                             prediction = forecasted_gcc[[1]],
-                             variable = "gcc_90")
-      forecast_df<- as.data.frame(rbind(forecast_df, curr_site_df)) 
-      
-      #the targets data only goes to 2024, remove the na.rm() thing from the targets.m call.
     }
   }
   print(paste(focal_sites[s], " forecast run"))
 }
-write.csv(forecast_df, file = "forecast_df.csv")
 
 
-ggplot(forecast_df, aes(datetime, prediction, group=parameter, color=site_id)) + geom_line()
+#aggregate data to account or overlap
+forecast_df_agg <- forecast_df |> 
+  group_by(datetime, parameter) |> 
+  mutate(prediction = mean(prediction)) 
+
+forecast_df_avg <- forecast_df_agg |> 
+  group_by(datetime, parameter) |> 
+  summarize(prediction = mean(prediction))
+
+ggplot(forecast_df_avg, aes(datetime, prediction, group=parameter, color=parameter)) + geom_line()
 #---- Covert to EFI standard ----
-
 # Make forecast fit the EFI standards
-forecast_df_EFI <- forecast_df %>%
-  filter(datetime > forecast_date) %>%
+forecast_df_EFI <- forecast_df_agg %>%
+  filter(datetime > "2024-01-01") %>%
   mutate(model_id = model_id,
          family = 'ensemble',
          duration = 'P1D',
          parameter = as.character(parameter),
          project_id = 'neon4cast') %>%
   select(datetime, reference_datetime, duration, site_id, family, parameter, variable, prediction, model_id, project_id)
-#---------------------------#
-
-
-
-# ----- Submit forecast -----
-# Write the forecast to file
-theme <- 'phenology'
-date <- forecast_df_EFI$reference_datetime[1]
-forecast_name <- paste0(forecast_df_EFI$model_id[1], ".csv")
-forecast_file <- paste(theme, date, forecast_name, sep = '-')
-
-write_csv(forecast_df_EFI, forecast_file)
-
-neon4cast::forecast_output_validator(forecast_file)
-
-
-neon4cast::submit(forecast_file =  forecast_file, ask = FALSE) # if ask = T (default), it will produce a pop-up box asking if you want to submit
-
-#--------------------------#
 
 forecast_df_EFI |> 
   ggplot(aes(x=datetime, y=prediction, group = parameter)) +
@@ -287,9 +273,7 @@ forecast_df_EFI |>
   theme_bw(base_size = 10) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
-plot_file_name <- paste0("Submit_forecast/", forecast_df_EFI$variable[1], '-', forecast_df_EFI$reference_datetime[1], ".png")
-ggsave(plot_file_name)
-
+write.csv(forecast_df_EFI, file = "reforecast_df.csv")
 
 #References:
 #https://rpubs.com/GChirinos/Tuning_Random_Forest
